1e40247973353a6d5aa0088b0e22b19a023df35e
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2017-10-27 17:55:44 -0300

    Responsive corrections

--- services.php
+++ services.php
@@ -13,7 +13,7 @@
             </div>
 
             <div class="pure-u-12-12 pure-u-md-7-12 padding-mb">
-              <h2 class="hero-section-description custom-animate">We help you elevate your business by taking your product to the next level through consult and the craft of design. We love walking the thin line between stunning and usable products.
+                <h2 class="hero-section-description custom-animate">We help you elevate your business by taking your product to the next level through consult and the craft of design. We love walking the thin line between stunning and usable products.</h2>
             </div>
 
           </div>
