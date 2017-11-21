d380a5d6a50eafd388b66e1c3fcba6a43611d247
sofiasoldano <30936938+sofiasoldano@users.noreply.github.com>
2017-11-21 17:27:33 -0300

    Corrections in rules and process sliders

--- services.php
+++ services.php
@@ -130,70 +130,72 @@
     </section>
 
     <section class="process-section">
-			<div class="process-number">1</div>
-	  	<div class="left-slide">
-				<div class="arrow-1"></div>
-				<div class="arrow-2"></div>
-				<div class="arrow-3"></div>
-			</div>
-				
-      <div class="pure-u-12-12 pure-u-md-5-12">
-		  <div class="process-absolute">
-			  <h2 class="process-subtitle">Our process</h2>
-			  <h1 class="process-title">Learning</h1>	 
-		 </div>
-	  </div>
-		
-	  <div class="pure-u-12-12 pure-u-md-7-12">
-				<div class="flexslider">
-						<ol class="flex-control-nav flex-control-paging">
-							<li><a id="1" class="flex-active">1</a></li>
-							<li><a id="2">2</a></li>
-							<li><a id="3">3</a></li>
-							<li><a id="4">4</a></li>
-							<li><a id="5">5</a></li>
-						</ol>
+         <div class="left-slide">
+                    <div class="arrow-1"></div>
+                    <div class="arrow-2"></div>
+                    <div class="arrow-3"></div>
+                </div>
+        <div class="process-container">
+            <div class="process-number">1</div>
+           
 
-					<ul class="slides">
-						<li class="main-slide"> 
-							<div class="process-info">
-								<h3 class="process-info-title">1/5 <span>Learning</span></h3>
-								<p class="process-info-description">We learn and analyze the business and its customers, their competence, their needs and challenges. We define a goal and metrics.</p>
-							</div>
-						</li>
-						<li class="last-slide">
-							<div class="process-info">
-								<h3 class="process-info-title">2/5 <span>Strategy</span></h3>
-								<p class="process-info-description">We work with the client on different action plans. We define a work plan with their respective tasks and responsible for each stage.</p>
-								</div>
-						</li>
-						<li>
-							<div class="process-info">
-								<h3 class="process-info-title">3/5 <span>Design</span></h3>
-								<p class="process-info-description">We iterate with the client on wireframes, sketches and initial designs based on the defined business strategy to create attractive and functional solutions. </p>
-							</div>
-						</li>
-						<li>
-							<div class="process-info">
-								<h3 class="process-info-title">4/5 <span>Development</span></h3>
-								<p class="process-info-description">We develop based on the latest web design and programming standards and validate the product with the customer prior to its launch.</p>
-							</div>
-						</li>
-						<li class="first-slide">
-							<div class="process-info">
-								<h3 class="process-info-title">5/5 <span>Launch</span></h3>
-								<p class="process-info-description">We launch the finished product and validate with the end user to detect new needs and learn about new goals.</p>
-							</div>
-						</li>
-					</ul>
-				</div>
-	  </div>
-				
-		<div class="right-slide">
-			<div class="arrow-1"></div>
-			<div class="arrow-2"></div>
-			<div class="arrow-3"></div>
-		</div>
+          <div class="pure-u-12-12 pure-u-md-5-12">
+              <div class="process-absolute">
+                  <h2 class="process-subtitle">Our process</h2>
+                  <h1 class="process-title">Learning</h1>	 
+             </div>
+          </div>
+
+          <div class="pure-u-12-12 pure-u-md-7-12">
+                    <div class="flexslider">
+                            <ol class="flex-control-nav flex-control-paging">
+                                <li><a id="1" class="flex-active">1</a></li>
+                                <li><a id="2">2</a></li>
+                                <li><a id="3">3</a></li>
+                                <li><a id="4">4</a></li>
+                                <li><a id="5">5</a></li>
+                            </ol>
+
+                        <ul class="slides">
+                            <li class="main-slide"> 
+                                <div class="process-info">
+                                    <h3 class="process-info-title">1/5 <span>Learning</span></h3>
+                                    <p class="process-info-description">We learn and analyze the business and its customers, their competence, their needs and challenges. We define a goal and metrics.</p>
+                                </div>
+                            </li>
+                            <li class="last-slide">
+                                <div class="process-info">
+                                    <h3 class="process-info-title">2/5 <span>Strategy</span></h3>
+                                    <p class="process-info-description">We work with the client on different action plans. We define a work plan with their respective tasks and responsible for each stage.</p>
+                                    </div>
+                            </li>
+                            <li>
+                                <div class="process-info">
+                                    <h3 class="process-info-title">3/5 <span>Design</span></h3>
+                                    <p class="process-info-description">We iterate with the client on wireframes, sketches and initial designs based on the defined business strategy to create attractive and functional solutions. </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="process-info">
+                                    <h3 class="process-info-title">4/5 <span>Development</span></h3>
+                                    <p class="process-info-description">We develop based on the latest web design and programming standards and validate the product with the customer prior to its launch.</p>
+                                </div>
+                            </li>
+                            <li class="first-slide">
+                                <div class="process-info">
+                                    <h3 class="process-info-title">5/5 <span>Launch</span></h3>
+                                    <p class="process-info-description">We launch the finished product and validate with the end user to detect new needs and learn about new goals.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+          </div>
+        </div>
+        <div class="right-slide">
+                <div class="arrow-1"></div>
+                <div class="arrow-2"></div>
+                <div class="arrow-3"></div>
+            </div>
   </section>
 			
 	 <section class="testimonials">
